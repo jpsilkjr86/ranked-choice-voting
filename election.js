070c9f36292995a7e4ca2a7dbfcfa360d520a99d
John Silkey <jpsilkjr86@gmail.com>
2017-12-02 08:18:53 -0700

    returned copy of object rather than object literal in createElection. added clearer comments. wrote some tests to check if data can be mutated when creating multiple elections. so far so good.

--- election.js
+++ election.js
@@ -1,6 +1,9 @@
+
 // createElection takes in choices as arguments and returns an object with methods
 // whose state consists of private variables (achieved by creating a closure)
 function createElection (...args) {
+
+	// **************** PRIVATE VARIABLES IN CLOSURE ****************
 	// creates private array "choices" as new array equal to ...args
 	const choices = [...args];
 	// votes array initialized as an empty array
@@ -15,14 +18,13 @@ function createElection (...args) {
 		return Object.assign({}, prevChoices, {[currentChoice]: 0});
 	}, {}); // initial value is empty object
 
-	// returns object with methods that have privileged access to private data above
-	return {
+	// ***************************************************************
+
+	// *********************** OBJECT TO RETURN ***********************
+	const election = {
 		// returns copy of array rather than ref. to it so it can't be mutated
 		getChoices() {
 			return [...choices];
-		},
-		addRankedVote(vote) {
-			votes.push(vote);
 		},
 		getVotes() {
 			// returns deep copy of votes array (array of arrays) to avoid mutation
@@ -31,9 +33,17 @@ function createElection (...args) {
 		getTally() {
 			// returns object copy of tally
 			return Object.assign({}, tally);
+		},
+		addRankedVote(vote) {
+			votes.push(vote);
 		}
-	}
-}
+	}; // end of election object
+	// ***************************************************************
+
+	// returns object prototypically inheriting election methods that
+	// have privileged access to private data above.
+	return Object.assign({}, election);
+} // end of createElection()
 
 module.exports = createElection;
 
--- sample-election.js
+++ sample-election.js
@@ -19,7 +19,36 @@ console.log('\ntally:\n', election.getTally());
 
 
 
-// // for testing to see if original data can be mutated (result: it can't)
+
+
+
+
+
+
+// // for testing to see if mutating election data would mess with other data
+// election.foo = 'bar';
+
+// console.log('election', election);
+// election.getChoices();
+
+// var test = createElection();
+// console.log('test', test);
+// test.bazz = 'bizz';
+
+// console.log('election', election);
+// election.getChoices();
+// console.log('test', test);
+// test.getChoices();
+
+
+
+
+
+
+
+
+
+// // for testing to see if original data can be mutated
 // const choices = election.getChoices();
 // const votes = election.getVotes();
 // const tally = election.getTally();
@@ -35,3 +64,19 @@ console.log('\ntally:\n', election.getTally());
 // console.log('\noriginal choices after', election.getChoices(), '\n');
 // console.log('\nvotes after:\n', election.getVotes());
 // console.log('\ntally after:\n', election.getTally());
+
+
+// // for testing to see if creating a new election have any side effects (mutation etc)
+// const election2 = createElection('banana1', 'banana2', 'banana3', 'banana4');
+
+// for (let i = 0; i < 10; i++) {
+// 	election2.addRankedVote(arrayShuffler(election2.getChoices()));
+// }
+
+// console.log('\noriginal choices2', election2.getChoices(), '\n');
+// console.log('\nvotes2:\n', election2.getVotes());
+// console.log('\ntally2:\n', election2.getTally());
+
+// console.log('\noriginal choices1 after', election.getChoices(), '\n');
+// console.log('\nvotes1 after:\n', election.getVotes());
+// console.log('\ntally1 after:\n', election.getTally());
