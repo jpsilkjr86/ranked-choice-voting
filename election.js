6b8fcb44568d575cebdf8385434f7bf437553ef3
John Silkey <jpsilkjr86@gmail.com>
2017-12-02 07:45:33 -0700

    added tally object in election.js and getTally() method. tested to make sure that mutating returned object will not result in mutation of private data in election closure

--- election.js
+++ election.js
@@ -5,8 +5,17 @@ function createElection (...args) {
 	const choices = [...args];
 	// votes array initialized as an empty array
 	const votes = [];
+	// tally is object with running total of votes for each candidate.
+	// it is initialized as an object whose keys are candidate names and
+	// whose values are their corresponding vote tally. Ex: {choice1: 0, choice2: 0}.
+	const tally = choices.reduce((prevChoices, currentChoice) => {
+		// reduce iterates over each value of choices and builds an object that
+		// consists of previous choices (key-value pairs of {choice: 0}) plus
+		// {currentChoice:0}.
+		return Object.assign({}, prevChoices, {[currentChoice]: 0});
+	}, {}); // initial value is empty object
 
-	// returns object with methods that have privileged access to "choices" array
+	// returns object with methods that have privileged access to private data above
 	return {
 		// returns copy of array rather than ref. to it so it can't be mutated
 		getChoices() {
@@ -18,8 +27,55 @@ function createElection (...args) {
 		getVotes() {
 			// returns deep copy of votes array (array of arrays) to avoid mutation
 			return JSON.parse(JSON.stringify(votes));
+		},
+		getTally() {
+			// returns object copy of tally
+			return Object.assign({}, tally);
 		}
 	}
 }
 
 module.exports = createElection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // createElection takes in choices as arguments and returns an object with methods
+// // whose state consists of private variables (achieved by creating a closure)
+// function createElection (...args) {
+// 	// creates private array "choices" as new array equal to ...args
+// 	const choices = [...args];
+// 	// votes array initialized as an empty array
+// 	const votes = [];
+
+// 	// returns object with methods that have privileged access to "choices" array
+// 	return {
+// 		// returns copy of array rather than ref. to it so it can't be mutated
+// 		getChoices() {
+// 			return [...choices];
+// 		},
+// 		addRankedVote(vote) {
+// 			votes.push(vote);
+// 		},
+// 		getVotes() {
+// 			// returns deep copy of votes array (array of arrays) to avoid mutation
+// 			return JSON.parse(JSON.stringify(votes));
+// 		}
+// 	}
+// }
+
+// module.exports = createElection;
--- sample-election.js
+++ sample-election.js
@@ -14,16 +14,24 @@ for (let i = 0; i < 10; i++) {
 
 console.log('\noriginal choices', election.getChoices(), '\n');
 console.log('\nvotes:\n', election.getVotes());
+console.log('\ntally:\n', election.getTally());
+
+
+
 
 // // for testing to see if original data can be mutated (result: it can't)
 // const choices = election.getChoices();
 // const votes = election.getVotes();
+// const tally = election.getTally();
 
 // choices[0] = 'asfaewfawefawefawfwf';
 // votes[0][0] = 'aaaaaaaaaaaaaaaaaaaa';
+// tally['Pizza'] = 17;
 // console.log(choices[0]);
 // console.log(votes[0][0]);
+// console.log(tally);
 
 
 // console.log('\noriginal choices after', election.getChoices(), '\n');
 // console.log('\nvotes after:\n', election.getVotes());
+// console.log('\ntally after:\n', election.getTally());
