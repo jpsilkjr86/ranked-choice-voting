00ad6fbea7c3be616557f2692c9c122f6fc5dbc6
John Silkey <jpsilkjr86@gmail.com>
2018-04-29 21:45:37 -0700

    added nodemon as dev-dep. added webpack-dev-middleware.js to abstract its configuration.

--- server.js
+++ server.js
@@ -9,32 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // ============ Webpack Middleware Configurations (Development Only - "npm run dev") ============
-// first checks to make sure NODE_ENV is in development mode
 if (process.env.NODE_ENV === 'development') {
-  console.log('NODE_ENV is in development mode.'
-  	+ '\nConfiguring webpack-dev-middleware and webpack-hot-middleware...');
-
-  // dependendies for webpack middleware
-  const webpack = require('webpack'),
-  	webpackDevMiddleware = require('webpack-dev-middleware'),
-  	webpackHotMiddleware = require('webpack-hot-middleware'),
-  	config = require('./webpack.dev.js');
-  const compiler = webpack(config);
-
-  // webpack-dev-middleware emits files compiled by webpack to a live server.
-  // webpack-hot-middleware allows hot reload of webpack with express server (just refresh page).
-  // more info about webpack-dev-middleware at:
-  // https://webpack.js.org/guides/development/#using-webpack-dev-middleware
-  // https://github.com/webpack/webpack-dev-middleware
-  // for info on integrating hot-middleware and dev-middleware see section "Server" at:
-  // https://ditrospecta.com/javascript/react/es6/webpack/heroku/2015/08/08/deploying-react-webpack-heroku.html
-
-  // configures webpack middlewares in development mode
-  app.use(webpackHotMiddleware(compiler));
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  }));
+  require('./webpack-dev-middleware')(app);
 }
 
 // ================ Express Configuration ================
---
+++ webpack-dev-middleware.js
@@ -0,0 +1,28 @@
+// export is a function that takes app as an argument
+module.exports = app => {
+	console.log('NODE_ENV is in development mode.'
+			+ '\nConfiguring webpack-dev-middleware and webpack-hot-middleware...');
+
+	// dependendies for webpack middleware
+	const webpack = require('webpack'),
+			webpackDevMiddleware = require('webpack-dev-middleware'),
+			webpackHotMiddleware = require('webpack-hot-middleware'),
+			config = require('./webpack.dev.js');
+	const compiler = webpack(config);
+
+	// webpack-dev-middleware emits files compiled by webpack to a live server.
+	// webpack-hot-middleware allows hot reload of webpack with express server (just refresh page).
+	// more info about webpack-dev-middleware at:
+	// https://webpack.js.org/guides/development/#using-webpack-dev-middleware
+	// https://github.com/webpack/webpack-dev-middleware
+	// for info on integrating hot-middleware and dev-middleware see section "Server" at:
+	// https://ditrospecta.com/javascript/react/es6/webpack/heroku/2015/08/08/deploying-react-webpack-heroku.html
+
+	// configures webpack middlewares in development mode
+	app.use(webpackHotMiddleware(compiler));
+	app.use(webpackDevMiddleware(compiler, {
+			noInfo: true,
+			publicPath: config.output.publicPath
+	}));
+}
+
