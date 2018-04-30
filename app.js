dda88d4318166cb5ceb5bcc73e070d336f94b01e
John Silkey <jpsilkjr86@gmail.com>
2018-04-29 23:03:39 -0700

    got rid of certain redundant comments

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-// ================ Dependencies ================
 const express = require('express'),
 	bodyParser = require('body-parser'),
   logger = require('morgan'),
   cookieParser = require('cookie-parser'),
   path = require('path');
 
-// sets up express app
+const webRouter = require('./routes/web'),
+  apiRouter = require('./routes/api');
+
 const app = express();
 
-// ============ Webpack Middleware Configurations (Development Only - "npm run dev") ============
 if (process.env.NODE_ENV === 'development') {
   require('./webpack-dev-middleware')(app);
 }
 
-// ================ Express Configuration ================
-const webRouter = require('./routes/web'),
-  apiRouter = require('./routes/api');
-
-// Configures Express and body parser
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,10 +20,8 @@ app.use(cookieParser());
 app.use(bodyParser.text());
 // app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-// serves public directory as static, enabling html pages to link with their assets
 app.use(express.static('public'));
 
-// set up routes
 app.use('/api', apiRouter);
 app.use('/', webRouter);
 
